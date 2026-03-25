2896eb1d479e9eda6b6c21bb5b13f1cbf377a547
ghsbnb <eziekial.bugdennweke@education.nsw.gov.au>
2026-03-25 15:38:53 +1100

    defining requests
    done on 11th of march

--- functions.py
+++ functions.py
@@ -16,4 +16,14 @@ def get_city_coordinates(city):
         raise WeatherAppError('City not found')
 
     result = res["results"][0]
-    return result
+        return {
+        "name": result["name"],
+        "latitude": result["latitude"],
+        "longitude": result["longitude"],
+    }
+
+def get_weather(lat, lon):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    res = requests.get(url).json()
+
+    return res["current_weather"]
